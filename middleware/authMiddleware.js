c148a3e1d6b2a2483f6031e06cdee532a1143ea3
ARDoagooei <a.r.doagooei@gmail.com>
2025-03-10 09:30:13 +0100

    adding middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+import { User } from '../models'; // Adjust path as needed
+
+const authMiddleware = async (req, res, next) => {
+    const token = req.header('Authorization');
+    if (!token)
+        return res
+            .status(401)
+            .json({ message: 'Access denied. No token provided.' });
+
+    try {
+        const decoded = jwt.verify(
+            token.replace('Bearer ', ''),
+            process.env.JWT_SECRET
+        );
+        req.user = await User.findByPk(decoded.id);
+        if (!req.user)
+            return res.status(404).json({ message: 'User not found' });
+        next();
+    } catch (error) {
+        res.status(400).json({ message: 'Invalid token' });
+    }
+};
+
+export default authMiddleware;
