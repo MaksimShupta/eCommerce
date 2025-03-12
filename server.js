1e00bc53b561b452844826fddf452eeaacd37599
ARDoagooei <a.r.doagooei@gmail.com>
2025-03-12 16:29:46 +0100

    Cleaning server.js

--- server.js
+++ server.js
@@ -1,35 +1,26 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './db/index.js';
+
+//importing routes
 import categoryRouter from './router/categoryRouter.js';
 import orderRouter from './router/orderRouter.js';
 import productRouter from './router/productRouter.js';
 import userRouter from './router/userRouter.js';
 
+//importing middleware
 import errorHandler from './middleware/errorHandler.js';
 import limiter from './middleware/limiter.js';
 import logger from './middleware/logger.js';
 
 dotenv.config();
 const PORT = process.env.PORT || 5001;
-
 const app = express();
 
 // Middlewares
 app.use(express.json());
 app.use(logger);
 app.use(limiter);
-
-app.get('/', (req, res) => {
-    res.send('hello world from nodejs!!!!!');
-});
-
-// Example: Protect a route
-// app.get("/profile", authMiddleware, (req, res) => {
-//     res.json({ user: req.user });
-// });
-
-// Start the server
 
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
